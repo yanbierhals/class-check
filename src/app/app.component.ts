b8728466c23b6d045884a1336139695bd08bc980
Yan Bierhals <yan-bierhals@procergs.rs.gov.br>
2025-04-23 10:29:51 -0300

    qrcode generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal, effect } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { QRCodeComponent } from 'angularx-qrcode';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [CommonModule, QRCodeComponent],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'class-check';
+  counter = signal(1);               // Valor do QR code
+  secondsLeft = signal(20);          // Contador regressivo de segundos
+
+  constructor() {
+    setInterval(() => {
+      this.counter.update((val) => val + 1);   // Atualiza QR
+      this.secondsLeft.set(20);                // Reseta contador
+    }, 20000);
+
+    setInterval(() => {
+      this.secondsLeft.update((val) => val > 0 ? val - 1 : 0); // Decrementa a cada segundo
+    }, 1000);
+  }
 }
