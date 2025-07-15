bb04ca2000ceedefe8e895d9986de1ac0d2501c8
oThisgo <thiagosds1701@gmail.com>
2025-07-15 19:32:21 -0300

    token fix

--- frontend/src/app/services/auth.interceptor.ts
+++ frontend/src/app/services/auth.interceptor.ts
@@ -2,13 +2,12 @@ import { HttpInterceptorFn } from '@angular/common/http';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
 
-  const authToken = localStorage.getItem('authToken');
+  const authToken = localStorage.getItem('token');
 
   if (authToken) {
     const authReq = req.clone({
       headers: req.headers.set('Authorization', `Bearer ${authToken}`)
     });
-
     return next(authReq);
   }
 
